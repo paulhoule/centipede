399cff5885292fd772e4abdfd248f0d704f651a2
Paul Houle <paul@ontology2.com>
2013-11-29 12:41:53 -0500

    resolve #6 this is working well enough to support the immediate use cases in Infovore

--- centipede/src/test/java/com/ontology2/centipede/parser/DirectoryExample.java
+++ centipede/src/test/java/com/ontology2/centipede/parser/DirectoryExample.java
@@ -19,25 +19,44 @@ public class DirectoryExample implements HasOptions {
     @Option(description="left",contextualConverter=DirectoryExample.LeftSubstitutor.class)
     public List<String> left;
 
-    @Option(description="right")
+    @Option(description="right",contextualConverter=DirectoryExample.RightSubstitutor.class)
     public List<String> right;
 
-    @Option(description="output")
+    @Option(description="output",contextualConverter=DirectoryExample.OutputSubstitutor.class)
     public String output;
 
-    public static class LeftSubstitutor implements ContextualConverter<String> {
-
+    public abstract static class DirectorySubstitutor<T> implements ContextualConverter<String> {
         public String convert(String value, HasOptions that) {
-            String defaultDir=((DirectoryExample) that).leftDir;
-            if(defaultDir.isEmpty()) {
-                defaultDir=((DirectoryExample) that).dir;
-            }
+            String defaultDir=getDefaultDir((T) that);
 
             if(defaultDir.isEmpty())
                 return value;
 
             File there=new File(defaultDir,value);
             return there.getPath();
+        }
+
+        public abstract String getDefaultDir(T that);
+    }
+
+    public static class LeftSubstitutor extends DirectorySubstitutor<DirectoryExample> {
+        @Override
+        public String getDefaultDir(DirectoryExample that) {
+            return that.leftDir.isEmpty() ? that.dir : that.leftDir;
+        }
+    }
+
+    public static class RightSubstitutor extends DirectorySubstitutor<DirectoryExample> {
+        @Override
+        public String getDefaultDir(DirectoryExample that) {
+            return that.rightDir.isEmpty() ? that.dir : that.rightDir;
+        }
+    }
+
+    public static class OutputSubstitutor extends DirectorySubstitutor<DirectoryExample> {
+        @Override
+        public String getDefaultDir(DirectoryExample that) {
+            return that.outputDir.isEmpty() ? that.dir : that.outputDir;
         }
     }
 }
--- centipede/src/test/java/com/ontology2/centipede/parser/TestDirectoryExample.java
+++ centipede/src/test/java/com/ontology2/centipede/parser/TestDirectoryExample.java
@@ -20,21 +20,60 @@ public class TestDirectoryExample {
     OptionParser directoryExampleParser;
 
     @Test
-    public void substituteIntoLeft() throws IllegalAccessException {
+         public void substituteIntoLeft() throws IllegalAccessException {
         List<String> args= Lists.newArrayList(
-            "-dir"
-            ,"/var/left/"
-            ,"-left"
-            ,"chomsky,albert,zinn"
+                "-dir"
+                ,"/var/left/"
+                ,"-left"
+                ,"chomsky,albert,zinn"
         );
 
         DirectoryExample ex=(DirectoryExample) directoryExampleParser.parse(args);
         List<String> expectedleft=Lists.newArrayList(
-            new File("/var/left/chomsky").toString(),
-            new File("/var/left/albert").toString(),
-            new File("/var/left/zinn").toString()
+                new File("/var/left/chomsky").toString(),
+                new File("/var/left/albert").toString(),
+                new File("/var/left/zinn").toString()
         );
 
         assertEquals(expectedleft, ex.left);
+    }
+
+    @Test
+    public void substituteIntoAll() throws IllegalAccessException {
+        List<String> args= Lists.newArrayList(
+                "-leftDir"
+                ,"/var/left/"
+                ,"-rightDir"
+                ,"/var/right/"
+                ,"-dir"
+                ,"/var/default"
+                ,"-outputDir"
+                ,"/var/output/"
+                ,"-left"
+                ,"chomsky,albert,zinn"
+                ,"-right"
+                ,"buchanan,buckley,vonMises"
+                ,"-output"
+                ,"differences"
+        );
+
+        DirectoryExample ex=(DirectoryExample) directoryExampleParser.parse(args);
+
+        List<String> expectedleft=Lists.newArrayList(
+                new File("/var/left/chomsky").toString(),
+                new File("/var/left/albert").toString(),
+                new File("/var/left/zinn").toString()
+        );
+
+
+        List<String> expectedright=Lists.newArrayList(
+                new File("/var/right/buchanan").toString(),
+                new File("/var/right/buckley").toString(),
+                new File("/var/right/vonMises").toString()
+        );
+
+        assertEquals(expectedright, ex.right);
+        assertEquals(expectedleft, ex.left);
+        assertEquals(new File("/var/output/differences").toString(),ex.output);
     }
 }
