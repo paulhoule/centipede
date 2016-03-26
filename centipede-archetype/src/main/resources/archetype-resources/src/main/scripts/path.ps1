c3bdf003c5c300fe18cc19c9e0eb49cc59054c5f
Paul Houle <paul@ontology2.com>
2016-03-26 12:40:08 -0400

    update and test centipede-archetype;  remove template connected with one-jar,  which we are no longer using,  also add the path to the Main class to make it easier to work with exec:java,  which is how we've been starting centipede projects for the past year ot so.

--- centipede-archetype/src/main/resources/archetype-resources/src/main/scripts/path.ps1
+++
@@ -1,6 +0,0 @@
-#set( $symbol_dollar = '$' )
-#set( $slashedGroup = $groupId.replace('.','\\') )
-
-function ${artifactId} {
-   java -jar ${symbol_dollar}env:userprofile\.m2\repository\\${slashedGroup}\\${artifactId}\\${symbol_dollar}{project.version}\\${artifactId}-${symbol_dollar}{project.version}-onejar.jar @args
-}
--- centipede-archetype/src/main/resources/archetype-resources/src/main/scripts/path.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#set( $symbol_dollar = '$' )
-#set( $slashedGroup = $groupId.replace(".","/") )
-alias ${artifactId}="java -jar $HOME/.m2/repository/${slashedGroup}/${artifactId}/${symbol_dollar}{project.version}/${artifactId}-${symbol_dollar}{project.version}-onejar.jar"
